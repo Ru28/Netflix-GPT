d2f01fe3e217571557d75a87414371b615a76b9e
Rupesh Virani <rupeshvirani01@gmail.com>
2024-07-03 18:50:31 +0530

    Mulit Language Support in GPT Search Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,15 @@ import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
-import { LOGO } from '../utils/constants';
+import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constants';
+import { toggleGptSearchView } from '../utils/gptSlice';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch= useDispatch();
   const user = useSelector(store => store.user)
+  const showGptSearch = useSelector(store =>store.gpt.showGptSearch)
 
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -33,6 +36,16 @@ const Header = () => {
       navigate("/error")
     });
   }
+
+  const handleGptSearchClick = () => {
+       dispatch(toggleGptSearchView())
+  }
+
+  const handleLanguageChange = (e)=>{
+     dispatch(changeLanguage(e.target.value))
+
+  }
+
   return (
     <div className="absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
       <img
@@ -40,8 +53,18 @@ const Header = () => {
         src= {LOGO}
         alt='logo'
       />
-      {user && (<div className="flex p-2">
-         <img className="w-12 h-12" src={user?.photoURL} alt='userIcon' />
+      {user && (
+      <div className="flex p-2">
+        {showGptSearch && (<select className='p-2 m-2 bg-gray-900 text-white' onChange={handleLanguageChange}>
+          {SUPPORTED_LANGUAGES.map((lang)=><option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
+        </select>)}
+        <button 
+          className='py-2 px-4 m-2 bg-purple-800 text-white rounded-lg'
+          onClick={handleGptSearchClick}
+          >
+            {showGptSearch? "HomePage": "GPT Search"}
+          </button>
+        <img className="w-12 h-12" src={user?.photoURL || "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"} alt='userIcon' />
         <button onClick={handleSignOut} className="font-bold text-white">(Sign Out)</button>
       </div>)}
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import MovieCard from './MovieCard'
 
 const MovieList = ({title, movies}) => {
-    console.log(movies,"hello")
     return (
         movies &&(<div className="px-6">
             <h1 className="text-3xl py-4 text-white">{title}</h1>
